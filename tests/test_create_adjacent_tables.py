5321775b9345f77b21d5709d73e6e623dd9be1e2
Dr Griffith Rees <griff.rees@gmail.com>
2023-07-03 15:48:51 +0100

    fix(test): improve `pytest.mark.xfail(reason=` descriptions

--- tests/test_create_adjacent_tables.py
+++ tests/test_create_adjacent_tables.py
@@ -3,10 +3,11 @@ from alto2txt2fixture import create_adjacent_tables
 import pytest
 
 
-@pytest.mark.xfail(reason="Known syntax error")
+@pytest.mark.xfail(reason="str/Path error in download_data call")
 def test_run() -> None:
     create_adjacent_tables.run()
 
 
+@pytest.mark.xfail(reason="str/Path error")
 def test_download_mitchells() -> None:
     create_adjacent_tables.download_data()
