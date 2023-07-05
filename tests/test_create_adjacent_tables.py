fe8ff46fb920b6bf999ccaae0104eb228c39fb2b
AoifeHughes <ahughes@turing.ac.uk>
2023-07-05 17:59:22 +0100

    pre-com fix

--- tests/test_create_adjacent_tables.py
+++ tests/test_create_adjacent_tables.py
@@ -6,5 +6,6 @@ from alto2txt2fixture import create_adjacent_tables
 def test_run() -> None:
     create_adjacent_tables.run()
 
+
 def test_download_mitchells(uncached_folder) -> None:
     create_adjacent_tables.download_data()
